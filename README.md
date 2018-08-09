4bba269719a813b87d8db65565793a76a5d10fe7
Dan Sedlacek <danfsedlacek@gmail.com>
2018-08-08 20:27:48 -0700

    lexer

---
+++ README.md
@@ -0,0 +1,2 @@
+This is me following along to the *Writing An Interpreter In Go* book.
+I guess I am just C+P code examples in the book ^_^
---
+++ token/token.go
@@ -0,0 +1,25 @@
+package token
+
+const (
+	ILLEGAL   = "ILLEGAL"
+	EOF       = "EOF"
+	IDENT     = "IDENT"
+	INT       = "INT"
+	ASSIGN    = "="
+	PLUS      = "INT"
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	FUNCTION  = "FUNCTION"
+	LET       = "LET"
+)
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
