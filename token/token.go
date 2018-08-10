69700f8f766d4d787afbc836aa161dd39c4a3ef4
Dan Sedlacek <danfsedlacek@gmail.com>
2018-08-09 21:23:32 -0700

    add more to the lexer

--- token/token.go
+++ token/token.go
@@ -1,5 +1,12 @@
 package token
 
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
 const (
 	ILLEGAL   = "ILLEGAL"
 	EOF       = "EOF"
@@ -7,19 +14,41 @@ const (
 	INT       = "INT"
 	ASSIGN    = "="
 	PLUS      = "INT"
+	MINUS     = "-"
+	BANG      = "!"
+	ASTERISK  = "*"
+	SLASH     = "/"
 	COMMA     = ","
 	SEMICOLON = ";"
 	LPAREN    = "("
 	RPAREN    = ")"
 	LBRACE    = "{"
 	RBRACE    = "}"
+	LT        = "<"
+	GT        = ">"
 	FUNCTION  = "FUNCTION"
 	LET       = "LET"
+	IF        = "IF"
+	ELSE      = "ELSE"
+	TRUE      = "TRUE"
+	FALSE     = "FALSE"
+	RETURN    = "RETURN"
 )
 
-type TokenType string
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+	"true":   TRUE,
+	"false":  FALSE,
+}
 
-type Token struct {
-	Type    TokenType
-	Literal string
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
 }
