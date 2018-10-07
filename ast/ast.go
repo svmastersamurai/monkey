abfea92edb244d0da8428876e80f9c262562258c
Dan Sedlacek <danfsedlacek@gmail.com>
2018-10-06 21:10:17 -0700

    add infix expression parsing

--- ast/ast.go
+++ ast/ast.go
@@ -25,6 +25,13 @@ type Program struct {
 	Statements []Statement
 }
 
+type InfixExpression struct {
+	Token    token.Token
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
 func (p *Program) String() string {
 	var out bytes.Buffer
 
@@ -134,6 +141,20 @@ func (pe *PrefixExpression) String() string {
 	out.WriteString("(")
 	out.WriteString(pe.Operator)
 	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+func (oe *InfixExpression) expressionNode()      {}
+func (oe *InfixExpression) TokenLiteral() string { return oe.Token.Literal }
+func (oe *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(oe.Left.String())
+	out.WriteString(" " + oe.Operator + " ")
+	out.WriteString(oe.Right.String())
 	out.WriteString(")")
 
 	return out.String()
